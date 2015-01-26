3cbc634ad26fc0b47ac3cfc9574df8d1fc9d529a
omryhay-fiverr <omry.h@fiverr.com>
2015-01-26 13:23:24 +0200

    Validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,4 @@
 class Micropost < ActiveRecord::Base
 	belongs_to :user
-	validates :content, length: { maximum: 140, minimum: 10}
+	validates :content, length: { maximum: 140, minimum: 10}, presence: true
 end
